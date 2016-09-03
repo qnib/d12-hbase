d946c8cca45c32502e0d0b5794b58e0e69761d12
Christian Kniep <christian@qnib.org>
2016-09-03 08:52:15 +0200

    add more hbase healthchecks

--- opt/qnib/hbase/bin/healthcheck.sh
+++ opt/qnib/hbase/bin/healthcheck.sh
@@ -6,6 +6,15 @@ fi
 if [ "${HADOOP_HDFS_NAMENODE}" == "true" ];then
     /opt/qnib/hdfs/namenode/bin/healthcheck.sh
 fi
+if [ "${HBASE_MASTER}" == "true" ];then
+    /opt/qnib/hbase/master/bin/healthcheck.sh
+fi
+if [ "${HBASE_REGIONSERVER}" == "true" ];then
+    /opt/qnib/hbase/regionserver/bin/healthcheck.sh
+fi
+if [ "${HBASE_REST}" == "true" ];then
+    /opt/qnib/hbase/rest/bin/healthcheck.sh
+fi
 
 exit 0
 
